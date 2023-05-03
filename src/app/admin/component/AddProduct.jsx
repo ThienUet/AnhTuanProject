09e56bd843e326616a745ed13a0fa8c72e283709
ThienUet <thin102000@gmail.com>
2023-05-03 22:29:17 +0700

    UPDATE

--- src/app/admin/component/AddProduct.jsx
+++ src/app/admin/component/AddProduct.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Form, Input, InputNumber } from 'antd';
 export default function AddProduct() {
+  const [addProductForm] = Form.useForm();
+
+  const onSubmit = () => {
+
+  }
   return (
-    <div>AddProduct</div>
+    <div className='add-product'>
+      <Form form={addProductForm} onFinish={onSubmit}>
+        <Form.Item name='name' label='Tên sản phẩm'>
+          <Input placeholder='Điền tên sản phẩm'/>
+        </Form.Item>
+        <Form.Item name='price' label='Điền giá sản phẩm'>
+          <Input placeholder='Điền giá sản phẩm'/>
+        </Form.Item>
+        <Form.Item name='percent_sale' label='Phần trăm giảm giá'> 
+          <Input placeholder='Điền phần trăm giảm giá'/>
+        </Form.Item>
+        <Form.Item name='quantity' label='Số lượng'>
+          <InputNumber />
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
